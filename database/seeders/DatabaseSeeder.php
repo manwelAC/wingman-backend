c53d0b2ffef822d50ec26bc4b069592f57edc253
John Manuel Cuerdo <johnmanuelcuerdo@gmail.com>
2026-05-21 23:20:26 +0800

    update: include payment method type seeder at the database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GameRankTiersSeeder::class,
             PilotSeeder::class,
+            PaymentMethodTypeSeeder::class,
         ]);
     }
 }
